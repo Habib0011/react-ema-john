14d5b2cde51e27d416ffced4fe176ec766c176a4
Your Name01~Habib0011 <you@example.comhabiburrahman5236@gmail.com>
2023-12-13 07:45:41 +0600

    show cart data

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -7,9 +7,15 @@ const Cart = ({cart}) => {
     // const {cart} = props
     let total = 0;
     let totalShipping = 0;
+    let quantity =0
     for(const product of cart){
-        total = total + product.price
+        if(product.quantity === 0){
+            product.quantity = 1
+        }
+        // product.quantity = product.quantity || 1
+        total = total + product.price*product.quantity
         totalShipping = totalShipping + product.shipping
+        quantity = quantity + product.quantity
     }
     const tax = total / 100 * 7;
     const taxFloat = parseFloat(tax).toFixed(2)
@@ -20,7 +26,7 @@ const Cart = ({cart}) => {
     return (
         <div className='cart'>
                  <h4>Order summary</h4>
-                <p>Selected Item: {cart.length}</p>
+                <p>Selected Item: {quantity}</p>
                 <p>Total Price: ${total}</p>
                 <p>Total Shipping Charge: ${totalShipping}</p>
                 <p>Tax: ${taxFloat}</p>
